6a130d8b59cd23960219b773418c3c7f30623a4e
Matthew Conlen <matthew.conlen@nytimes.com>
2020-06-28 23:16:29 -0700

    work on pacing and layout

--- components/matt-note.js
+++ components/matt-note.js
@@ -4,8 +4,8 @@ class MattNote extends React.Component {
   render() {
     const { idyll, updateProps, hasError, ...props } = this.props;
     return (
-      <div stlye={{color: 'red', fontFamily: 'monospace'}}>
-        {props.children}
+      <div style={{color: '#aa4444', fontFamily: 'monospace'}}>
+        {props.children} <em>- Matt C.</em>
       </div>
     );
   }
